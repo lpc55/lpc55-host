3940a5c1ea2e303f338adfd76378dc225afb8e3d
nickray <nickray@users.noreply.github.com>
2020-12-12 00:24:38 +0000

    deploy: f1c7f2b889e5c43ca9366a4c56e014a314c6740a

--- lpc55/rotkh/sidebar-items.js
+++ lpc55/rotkh/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["calculate",""],["rot_key_hashes",""]],"struct":[["Config",""]]});
+initSidebarItems({"fn":[["calculate",""],["rot_key_hash","NXP-specific construction that gives a \"fingerprint\" of the certificate's public key"],["rot_key_hashes",""]],"struct":[["Config",""]]});
