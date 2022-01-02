fe24255d0a02d945ad5aa820d89324ea5aecd6bc
nickray <nickray@users.noreply.github.com>
2022-01-02 11:59:36 +0000

    deploy: 6ce1a3b944a10b48afd538795044fd8969e01611

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["lpc55"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"lpc55/bootloader/protocol/enum.Error.html\" title=\"enum lpc55::bootloader::protocol::Error\">Error</a>","synthetic":false,"types":["lpc55::bootloader::protocol::Error"]}];
+implementors["lpc55"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"lpc55/bootloader/protocol/enum.Error.html\" title=\"enum lpc55::bootloader::protocol::Error\">Error</a>","synthetic":false,"types":["lpc55::bootloader::protocol::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
