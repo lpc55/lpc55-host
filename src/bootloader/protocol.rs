lpc55/lpc55-host

commit 4a30beff829f489c10674cf2d729542644311a53

Clippy fixes

src/bootloader/protocol.rs
@@ -136,7 +136,7 @@ impl Protocol {
                 // successful status, mirroring our command header
                 let packet = ResponsePacket::try_from(initial_response)?;
 
-                assert_eq!(packet.has_data, false);
+                assert!(!packet.has_data);
                 if let Some(status) = packet.status {
                     panic!("{:?}", status);
                 }
@@ -196,7 +196,7 @@ impl Protocol {
                         }
 
                         let packet = ResponsePacket::try_from(self.read_packet()?)?;
-                        assert_eq!(packet.has_data, false);
+                        assert!(!packet.has_data);
                         if let Some(status) = packet.status {
                             panic!("unexpected status {:?}", &status);
                         }
@@ -226,7 +226,7 @@ impl Protocol {
                         }
 
                         let packet = ResponsePacket::try_from(self.read_packet()?)?;
-                        assert_eq!(packet.has_data, false);
+                        assert!(!packet.has_data);
                         if let Some(status) = packet.status {
                             panic!("unexpected status {:?}", &status);
                         }
@@ -255,7 +255,7 @@ impl Protocol {
                             x => x?,
                         })?;
                         // let packet = ResponsePacket::try_from(self.read_packet()?)?;
-                        assert_eq!(packet.has_data, false);
+                        assert!(!packet.has_data);
                         if let Some(status) = packet.status {
                             panic!("unexpected status {:?}", &status);
                         }
@@ -292,7 +292,7 @@ impl Protocol {
 
                 let packet = ResponsePacket::try_from(initial_response)?;
                 // assert_eq!([0x03, 0x00, 0x0C, 0x00], &initial_generic_response[..4]);
-                assert_eq!(packet.has_data, true);
+                assert!(!packet.has_data);
                 assert!(packet.status.is_none());
                 assert_eq!(packet.tag, command::ResponseTag::ReadMemory);
 
@@ -309,7 +309,7 @@ impl Protocol {
                 }
 
                 let packet = ResponsePacket::try_from(self.read_packet()?)?;
-                assert_eq!(packet.has_data, false);
+                assert!(!packet.has_data);
                 assert!(packet.status.is_none());
 
                 assert_eq!(packet.tag, command::ResponseTag::Generic);
@@ -395,7 +395,7 @@ impl Protocol {
         if sent >= all {
             Ok(())
         } else {
-            Err(hidapi::HidError::IncompleteSendError { sent, all })?
+            Err(hidapi::HidError::IncompleteSendError { sent, all }.into())
         }
     }
 

build.rs
@@ -29,7 +29,7 @@ fn main() {
     }
 
     // place side by side with binaries
-    let outdir = path::PathBuf::from(path::PathBuf::from(env_outdir).ancestors().skip(3).next().unwrap());
+    let outdir = path::PathBuf::from(path::PathBuf::from(env_outdir).ancestors().nth(3).unwrap());
     fs::create_dir_all(&outdir).unwrap();
     println!("{:?}", &outdir);
 

src/bin/lpc55/cli.rs
@@ -4,7 +4,7 @@ use clap::{self, crate_authors, crate_version, App, Arg, SubCommand};
 
 // duplicated here, because when using this `cli` module in build.rs
 // to generate shell completions, there is no `lpc55` crate yet
-pub const KEYSTORE_KEY_NAMES: [&'static str; 6] = [
+pub const KEYSTORE_KEY_NAMES: [&str; 6] = [
     "secure-boot-kek",
     "user-key",
     "unique-device-secret",

src/bin/lpc55/logger.rs
@@ -14,7 +14,7 @@ use log::{self, Log};
 #[derive(Debug)]
 pub struct Logger(());
 
-const LOGGER: &'static Logger = &Logger(());
+const LOGGER: &Logger = &Logger(());
 
 impl Logger {
     /// Create a new logger that logs to stderr and initialize it as the

src/bin/lpc55/main.rs
@@ -59,7 +59,7 @@ fn try_main(args: clap::ArgMatches<'_>) -> anyhow::Result<()> {
     if let Some(command) = args.subcommand_matches("http") {
         let bootloader = bootloader()?;
         let addr = command.value_of("ADDR").unwrap().to_string();
-        let port = u16::from_str_radix(command.value_of("PORT").unwrap(), 10).unwrap();
+        let port = command.value_of("PORT").unwrap().parse::<u16>().unwrap();
         let http_config = lpc55::http::HttpConfig { addr, port, timeout_ms: 5000 };
         let server = lpc55::http::Server::new(&http_config, bootloader)?;
         server.run()?;
@@ -169,13 +169,11 @@ fn try_main(args: clap::ArgMatches<'_>) -> anyhow::Result<()> {
                     info!("preserving firmware, prince-iv, and reserved fields.");
 
                     // Do not overwrite firmware versions
-                    for i in 8 .. 16 {
-                        settings[i] = current_pfr_raw[i];
-                    }
+                    let protect = 8..16;
+                    settings[protect.clone()].clone_from_slice(&current_pfr_raw[protect]);
                     // Do not overwrite any of the PRINCE IV's or reserved areas.
-                    for i in 48 .. 256 {
-                        settings[i] = current_pfr_raw[i];
-                    }
+                    let protect = 48..256;
+                    settings[protect.clone()].clone_from_slice(&current_pfr_raw[protect]);
                 }
 
                 trace!("writing pfr: {}", hex_str!(&settings));
@@ -445,8 +443,8 @@ fn try_main(args: clap::ArgMatches<'_>) -> anyhow::Result<()> {
         if let Some(product_date) = command.value_of("product-date") {
             use chrono::naive::NaiveDate;
             let date = NaiveDate::parse_from_str(product_date, "%Y-%m-%d")
-                .or(NaiveDate::parse_from_str(product_date, "%Y%m%d"))
-                .or(NaiveDate::parse_from_str(product_date, "%y%m%d"))?;
+                .or_else(|_| NaiveDate::parse_from_str(product_date, "%Y%m%d"))
+                .or_else(|_| NaiveDate::parse_from_str(product_date, "%y%m%d"))?;
             let days_since_twenties = (date - NaiveDate::from_ymd(2020, 1, 1)).num_days();
             assert!(days_since_twenties > 0);
             info!("overriding product.major with date {}, i.e. {}", &date, days_since_twenties);

src/bootloader.rs
@@ -106,9 +106,8 @@ impl Bootloader {
             })
             .filter_map(|(device, vid, pid)| {
                 let protocol = Protocol::new(device);
-                let bootloader = GetProperties { protocol: &protocol }.device_uuid().ok()
-                    .map(|uuid| Self { protocol, vid, pid, uuid });
-                bootloader
+                GetProperties { protocol: &protocol }.device_uuid().ok()
+                    .map(|uuid| Self { protocol, vid, pid, uuid })
             })
             .collect()
     }

src/bootloader/command.rs
@@ -69,10 +69,7 @@ pub enum DataPhase {
 
 impl DataPhase {
     pub fn has_command_data(&self) -> bool {
-        match self {
-            DataPhase::CommandData(_) => true,
-            _ => false,
-        }
+        matches!(self, DataPhase::CommandData(_))
     }
 }
 
@@ -93,8 +90,8 @@ impl Command {
                let mut bytes = Vec::with_capacity(words.len() * 4);
                let cursor = &mut bytes;
 
-               for i in 0 .. words.len() {
-                   cursor.write_all(&words[i].to_le_bytes()).unwrap();
+               for word in words.iter() {
+                   cursor.write_all(&word.to_le_bytes()).unwrap();
                }
 
                 DataPhase::CommandData(bytes)
@@ -483,7 +480,7 @@ pub enum Key {
 }
 
 // unfortunately duplicated in cli.rs, for why see there
-pub const KEYSTORE_KEY_NAMES: [&'static str; 6] = [
+pub const KEYSTORE_KEY_NAMES: [&str; 6] = [
     "secure-boot-kek",
     "user-key",
     "unique-device-secret",

src/bootloader/error.rs
@@ -1,4 +1,4 @@
-//! https://github.com/NXPmicro/spsdk/blob/020a983e53769fe16cb9b49395d56f0201eccca6/spsdk/mboot/error_codes.py
+//! <https://github.com/NXPmicro/spsdk/blob/020a983e53769fe16cb9b49395d56f0201eccca6/spsdk/mboot/error_codes.py>
 
 use core::convert::TryFrom;
 
@@ -125,10 +125,10 @@ generate! { CrcCheckerError:
     OutOfRange = 4,
 }
 
-impl Into<(ErrorGroup, u8)> for BootloaderError {
-    fn into(self) -> (ErrorGroup, u8) {
+impl From<BootloaderError> for (ErrorGroup, u8) {
+    fn from(error: BootloaderError) -> Self {
         use BootloaderError::*;
-        match self {
+        match error {
             Generic(error) => (ErrorGroup::Generic, error as u8),
             FlashDriver(error) => (ErrorGroup::FlashDriver, error as u8),
             PropertyStore(error) => (ErrorGroup::PropertyStore, error as u8),
@@ -142,8 +142,7 @@ impl Into<(ErrorGroup, u8)> for BootloaderError {
 impl From<BootloaderError> for u32 {
     fn from(error: BootloaderError) -> u32 {
         let (group, code) = error.into();
-        let status = (group as u32 * 100) + code as u32;
-        status
+        (group as u32 * 100) + code as u32
     }
 }
 
@@ -158,7 +157,7 @@ impl From<u32> for BootloaderError {
                 (ErrorGroup::PropertyStore, code) => PropertyStoreError::try_from(code).map_or(Unknown(status), PropertyStore),
                 (ErrorGroup::CrcChecker, code) => CrcCheckerError::try_from(code).map_or(Unknown(status), CrcChecker),
                 (ErrorGroup::SbLoader, code) => SbLoaderError::try_from(code).map_or(Unknown(status), SbLoader),
-                _ => return Unknown(status),
+                _ => Unknown(status),
             }
         } else {
             Unknown(status)

commit 71c2f9be0adb9dea085c5f2a2bc96eda87ab4972

Fix assert broken in 4a30bef

src/bootloader/protocol.rs
@@ -323,7 +323,7 @@ impl Protocol {
             (command::Command::ReadMemory { address: _, length }, _, _) => {
                 let packet = ResponsePacket::try_from(initial_response)?;
                 // assert_eq!([0x03, 0x00, 0x0C, 0x00], &initial_generic_response[..4]);
-                assert!(!packet.has_data);
+                assert!(packet.has_data);
                 assert!(packet.status.is_none());
                 assert_eq!(packet.tag, command::ResponseTag::ReadMemory);
 