4a4429522e81d841ab253c4d54e31b88eb3a67b9
nickray <nickray@users.noreply.github.com>
2023-01-16 23:21:42 +0000

    deploy: 7c334b4de555aeeb96ae432681916f781008c15b

--- implementors/signature/signature/trait.Signature.js
+++ implementors/signature/signature/trait.Signature.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["lpc55"] = [{"text":"impl Signature for <a class=\"struct\" href=\"lpc55/pki/struct.Signature.html\" title=\"struct lpc55::pki::Signature\">Signature</a>","synthetic":false,"types":["lpc55::pki::Signature"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"lpc55":[["impl Signature for <a class=\"struct\" href=\"lpc55/pki/struct.Signature.html\" title=\"struct lpc55::pki::Signature\">Signature</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["lpc55"] = {"name":"","dirs":[{"name":"bootloader","files":["command.rs","error.rs","property.rs","protocol.rs","provision.rs"]},{"name":"protected_flash","files":["debug.rs"]},{"name":"secure_binary","files":["command.rs"]}],"files":["bootloader.rs","crypto.rs","http.rs","lib.rs","pki.rs","protected_flash.rs","secure_binary.rs","signed_binary.rs","util.rs"]};
+var sourcesIndex = JSON.parse('{\
+"lpc55":["",[["bootloader",[],["command.rs","error.rs","property.rs","protocol.rs","provision.rs"]],["protected_flash",[],["debug.rs"]],["secure_binary",[],["command.rs"]]],["bootloader.rs","crypto.rs","http.rs","lib.rs","pki.rs","protected_flash.rs","secure_binary.rs","signed_binary.rs","util.rs"]]\
+}');
 createSourceSidebar();
